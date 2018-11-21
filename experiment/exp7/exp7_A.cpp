3607fa8ff92d54f85c421870782c8e507b1e4baf
stevenyangh <stevenyanmu@outlook.com>
2018-11-21 14:16:49 +0800

    whhshs

---
+++ experiment/exp7/exp7_A.cpp
@@ -0,0 +1,16 @@
+#include<stdio.h>
+
+int convert(int n)
+{
+    if(n > 0)
+	return n % 10 + 2 * convert(n / 10);
+    else return 0;
+}
+
+int main()
+{
+    int n;
+    scanf("%d", &n);
+    printf("%d\n", convert(n));
+    return 0;
+}
