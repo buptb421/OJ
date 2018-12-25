2ad08de691a7ca0a2396d1be298d600f1924c333
stevenyangh <stevenyanmu@outlook.com>
2018-12-25 17:43:32 +0800

    Exam 3 has been done.

--- exam/test3/test3_E.cpp
+++ exam/test3/test3_E.cpp
@@ -1,5 +1,5 @@
 #include<stdio.h>
-
+#define BINARY_SEARCH_LENGTH_MIN 5
 long long string[10000];
 
 int septest(long long testlen, int n);
@@ -18,13 +18,16 @@ int main()
     long long sta = 1, end = 2000000;
     binaryS(&sta, &end, n, m);
     int nSeg;
-    for(nSeg = 0; end > sta && nSeg < m; end--)
-      nSeg = septest(end, n);
-    printf("%lld\n", end);
+    for(nSeg = 0; end >= sta && nSeg < m; end--)
+    {
+	nSeg = septest(end, n);
+//	printf("%lld %d\n", end, nSeg);
+    }
+    printf("%lld\n", end + 1);
     
     return 0;
 }
-3
+
 int septest(long long testlen, int n)
 {
     int sum = 0;
@@ -35,7 +38,7 @@ int septest(long long testlen, int n)
 
 void binaryS(long long *i, long long *j, int n, int m)
 {
-    while((*j - *i) > 8)
+    while((*j - *i) >= BINARY_SEARCH_LENGTH_MIN)
     {
 	if(septest((*i + *j) / 2, n) >= m)
 	    *i = (*i + *j) / 2;
