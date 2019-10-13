3f4b86f3ac9f6ee33552b7bd3a43cb3970f4a2f6
stevenyangh <stevenyanmu@outlook.com>
2019-10-13 16:33:52 +0800

    rational sync

--- demonstration/character_strings_iter.cpp
+++ demonstration/character_strings_iter.cpp
@@ -13,74 +13,74 @@ void inputStr(char str[], const int buffer_length)
 
 int isEdge(char str[], int i)
 {
-	if(i < 0 or str[i] == '\0')
-		return 1;
-	else
-		return 0;
+    if(i < 0 or str[i] == '\0')
+	return 1;
+    else
+	return 0;
 }
 
 int isSep(char str[], int i)
 {
-	if(str[i] == ' ')
-		return 1;
-	else
-		return 0;
+    if(str[i] == ' ')
+	return 1;
+    else
+	return 0;
 }
 
 int reachEnd(char str[])
 {
-	int i = 0;
-	while(str[i] != '\0')
-	{
-		i++;	
-	}
-	return i - 1;	
+    int i = 0;
+    while(str[i] != '\0')
+    {
+	i++;	
+    }
+    return i - 1;	
 } 
 
 int readWord(char str[], int i, int step)
 {
-	int j;
-	for(j = i; !isEdge(str, j) && !isSep(str, j); j = j + step)
-		printf("%c", str[j]);
-	
-	return j;
+    int j;
+    for(j = i; !isEdge(str, j) && !isSep(str, j); j = j + step)
+	printf("%c", str[j]);
+    
+    return j;
 }
 
 int readSent(char str[], int i, int s_step, int w_step)
 {
-	int j;
-	if(s_step * w_step > 0)
+    int j;
+    if(s_step * w_step > 0)
+    {
+	for(j = i; !isEdge(str, j); j = j + s_step)
 	{
-		for(j = i; !isEdge(str, j); j = j + s_step)
-		{
-			if(!isSep(str, j))
-			{
-				j = readWord(str, j, w_step);
-				printf(" ");
-			}
-		}
-		printf("\n");
+	    if(!isSep(str, j))
+	    {
+		j = readWord(str, j, w_step) - s_step;
+		printf(" ");
+	    }
 	}
-	else
+	printf("\n");
+    }
+    else
+    {
+	int sep_handler;
+	for(j = i; !isEdge(str, j); j = j + s_step)
 	{
-		int sep_handler;
-		for(j = i; !isEdge(str, j); j = j + s_step)
+	    if(isSep(str, j))
+	    {
+		if(sep_handler == 0)
 		{
-			if(isSep(str, j))
-			{
-				if(sep_handler == 0)
-				{
-					sep_handler = 1;
-					readWord(str, j - s_step, w_step);
-					printf(" ");
-				}
-			}
-			else
-				sep_handler = 0;
+		    sep_handler = 1;
+		    readWord(str, j - s_step, w_step);
+		    printf(" ");
 		}
-		readWord(str, j - s_step, w_step);
-		printf("\n");
+	    }
+	    else
+		sep_handler = 0;
 	}
+	readWord(str, j - s_step, w_step);
+	printf("\n");
+    }
 }
 
 int main()
@@ -90,9 +90,9 @@ int main()
     printf("Input:");
     inputStr(ori, 300);
     
-	readSent(ori, 0, 1, 1);
-	readSent(ori, 0, 1, -1);
-	readSent(ori, reachEnd(ori), -1, 1);
+    readSent(ori, 0, 1, 1);
+    readSent(ori, 0, 1, -1);
+    readSent(ori, reachEnd(ori), -1, 1);
     readSent(ori, reachEnd(ori), -1, -1);
     
     return 0;
